3b081a0b499c84e776033e3596f4dd7868cabda1
Abhishek M <m.abhishek@sonata-software.com>
2024-07-31 14:44:06 +0530

    commit

--- scripts/install-package.ps1
+++ scripts/install-package.ps1
@@ -10,5 +10,5 @@ param (
 # Install choco
 # Get the previous version of the package installed
 # Install the new version
-choco install $msiName --version $version --source="${{ secrets.JFROG_ARTIFACTORY_URL}}api/nuget/${{secrets.JFROG_REPOSITORY}}/" -u=sharad1 -pwd=Sharad@123
-# Rollback the version
+        choco install $msiName --version $version --source="$jfrogarifactoryurl /api/nuget/${{secrets.JFROG_REPOSITORY}}/" -u=sharad1 -pwd=Sharad@123
+# Rollback the version if there is any error while installing new version
