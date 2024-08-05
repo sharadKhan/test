2f50262dbbf3a2ae322697fe96a5ab472aee2b0e
Abhishek M <m.abhishek@sonata-software.com>
2024-08-05 14:26:43 +0530

    commit

---
+++ packs/MSIDemo/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+$packageParameters = Get-PackageParameters
+$fileLocation = $packageParameters['filelocation']
+$defaultArgs = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+$arguments = $packageParameters['arguments'] -replace '%space%', ' '
+$finalArgs = "$defaultArgs $arguments"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'MSI'
+    file           = $fileLocation
+    softwareName   = 'MSIDemo'
+    checksum64     = 'D726C5E50B879FA0BF828A844A090181D8C4874A9209C5AC69BFB96EA6444BC6'
+    checksumType64 = 'sha256'
+
+    silentArgs     = $finalArgs
+    validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs 
+
