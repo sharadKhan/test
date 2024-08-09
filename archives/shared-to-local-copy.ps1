7a0c1e63d35e08322910ef0c869c611adb5f5a9c
Abhishek M <m.abhishek@sonata-software.com>
2024-08-09 12:04:52 +0530

    commit

--- archives/shared-to-local-copy.ps1
+++ archives/shared-to-local-copy.ps1
@@ -1,26 +1,23 @@
 # Define parameters
-$remoteIP = "172.29.74.24"       
+$remoteIP = "172.29.74.154"       
 $remoteUser = "m.abhishek@sonata-software.com"  
 $remotePassword = "Gv@123456"
-$sharedPath = "\\BG4PHS29EPCOVM1\shared\MSIDemo.msi"
-$localPath = "D:\MSIFiles"
+# $sharedPath = "\\BG4PHS29EPCOVM1\shared\MSIDemo.msi"
+# $localPath = "D:\MSIFiles"
 
-# # Create a PSCredential object for remote login
-# $securePassword = ConvertTo-SecureString $remotePassword -AsPlainText -Force
-# $credential = New-Object System.Management.Automation.PSCredential ($remoteUser, $securePassword)
+# Create a PSCredential object for remote login
+$securePassword = ConvertTo-SecureString $remotePassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($remoteUser, $securePassword)
 
-# # Script block to run on the remote machine
-# $scriptBlock = {
-#     param (
-#         [string]$sharedPath,
-#         [string]$localPath
-#     )
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value "10.210.222.182" -Force
 
-#     # Copy the file from the shared location to the local drive
-#     Copy-Item -Path $sharedPath -Destination $localPath -Force
-# }
+# Script block to run on the remote machine
+$scriptBlock = {
 
-# # Use Invoke-Command to run the script block on the remote machine
-# Invoke-Command -ComputerName $remoteIP -Credential $credential -ScriptBlock $scriptBlock -ArgumentList $sharedPath, $localPath
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Value "10.210.222.182" -Force
+}
+
+# Use Invoke-Command to run the script block on the remote machine
+Invoke-Command -ComputerName $remoteIP -Credential $credential -ScriptBlock $scriptBlock 
 # Copy-Item -Path $sharedPath -Destination $localPath -Force
-Copy-Item "${{ github.workspace }}\Readme.md" -Destination ""
+# Copy-Item "${{ github.workspace }}\Readme.md" -Destination ""
